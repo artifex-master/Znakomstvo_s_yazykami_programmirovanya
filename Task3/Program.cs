59922f0ad6b68c03b0d16c092764c9a5d4baf440
Konstantin Pak <artifex.konstantin.devit@outlook.com>
2024-03-15 13:00:04 +0600

    Домашнее Задание. Промежуточная аттестация.

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы
+int[] array = new int[]{12, 16, 8, 23, 30, 28, 20};
+int lastIndex = array.Length - 1;
+
+void PrintArrayElements(int[] array, int i)
+{
+
+  if(i < 0)
+  {
+    return;
+  } 
+
+  Console.Write($"{array[i]} ");
+  PrintArrayElements(array, i - 1);
+}
+
+PrintArrayElements(array, lastIndex);
