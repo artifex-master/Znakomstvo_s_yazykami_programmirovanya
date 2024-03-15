f17716620fea4b35289861dbe391bcc5f7e08941
Konstantin Pak <artifex.konstantin.devit@outlook.com>
2024-03-16 00:56:25 +0600

    Знакомство с языками программирования. Семинары. Промежуточная аттестация.

---
+++ Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("Введите значение переменной M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной N: ");
+int n = int.Parse(Console.ReadLine());
+
+int getMinNumber(int x, int y)
+{
+    int min;
+
+    if(x > y)
+    {
+        min = y;
+    }
+    else
+    {
+        min = x;
+    }
+
+    return min;
+}
+
+int getMaxNumber(int x, int y)
+{
+    int max;
+
+    if(x > y)
+    {
+        max = x;
+    }
+    else
+    {
+        max = y;
+    }
+
+    return max;
+}
+
+int min = getMinNumber(m, n);
+int max = getMaxNumber(m, n);
+
+void PrintNumbers(int current, int limit)
+{
+    if(current > limit)
+    {
+        return;
+    }
+
+    Console.Write($"{current} ");
+    PrintNumbers(current + 1, limit);
+
+}
+
+PrintNumbers(min, max);
